8ff6a6720cb991271ae51d2ed48754b8e1311e60
Garrett Vercoe <garrettvercoe@gmail.com>
2023-06-18 11:47:04 -0400

    upd

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,13 +22,12 @@ const IndexPage = () => {
       <CanvasGrid clicked={clicked}></CanvasGrid>
       <div className="text" style={ {  pointerEvents:clicked &&' none'}}>
         <h2>👋</h2>
-Spolia is a design-research lab building creative technologies. <br/>
-We design products with startups and help in their implementation.  <br/>
-Sometimes, we build cool things ourselves too.  <br/>
- <br/>
-Our mindful time-management app, <a href='https://apps.apple.com/us/app/hone-one-thing-at-a-time/id1624789090?mt=12' target="_blank"><span>Hone</span></a>, is now in public release on the Mac App Store. <br/>
+Spolia is an open-source design-research lab building creative technologies.
+<br/>
+Our mission is to bring academic research to the open public. <br/>
 
-If you want to chat about working with us, or want to join our design collaborative,  <br/>
+
+If you want to chat about joining our design collaborative,  <br/>
 contact us at <a href="mailto:hello@spolialab.com">hello@spolialab.com</a>. <br/> <br/>
 <span className="noMobile">
 If you just want something fun to do on this {new Date().toLocaleString('en-us', {  weekday: 'long' })},  <br/>
