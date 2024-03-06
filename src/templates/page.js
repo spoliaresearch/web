767d54841bef3d103e95f6532b1c1567b4fc609e
Garrett Vercoe <garrettvercoe@gmail.com>
2024-03-06 14:50:39 -0500

    comment out comments for now to pull from main plugin

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,58 +1,58 @@
-import {Link, graphql} from "gatsby"
-import {GatsbyImage, getImage} from "gatsby-plugin-image"
-import React, {useEffect} from "react"
-import './page.css'
+import { Link, graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import React, { useEffect } from "react";
+import "./page.css";
 const TemplatePage = ({
   data: {
     page: {
       name,
       cover,
-      childMarkdownRemark: {html},
-    comments
+      childMarkdownRemark: { html },
+      comments,
     },
   },
 }) => {
- useEffect(() => {
-  const element = document.getElementById("my-anchor-2");
-  element.scrollIntoView({ behavior: 'instant' });
-}, []);
+  useEffect(() => {
+    const element = document.getElementById("my-anchor-2");
+    element.scrollIntoView({ behavior: "instant" });
+  }, []);
 
   return (
     <div class="ProjectContent">
-  
       {/* <h1>{name}</h1> */}
-    
-      <div className="mainContent">
-  
-  <div dangerouslySetInnerHTML={{__html: html}} />
 
+      <div className="mainContent">
+        <div dangerouslySetInnerHTML={{ __html: html }} />
       </div>
 
-      <div className="context">  {comments.map(comment => (
+      <div className="context">
+        {" "}
+        {/* {comments.map((comment) => (
           <div key={comment.id}>
             <p>{comment.content}</p>
-            {/* {comment.replies.map(reply => (
+             {comment.replies.map(reply => (
               <p key={reply.id}>{reply.content}</p>
-            ))} */}
+            ))} 
           </div>
-        ))}</div>
-          
+        ))} */}
+      </div>
+
       {/*
         To add a cover:
         Add an image in your Google Doc first page header
         https://support.google.com/docs/answer/86629
       */}
       {/* {cover && <GatsbyImage image={getImage(cover.image)} />}
- */}
+       */}
     </div>
-  )
-}
+  );
+};
 
-export default TemplatePage
+export default TemplatePage;
 
 export const pageQuery = graphql`
   query Page($path: String!) {
-    page: googleDocs(slug: {eq: $path}) {
+    page: googleDocs(slug: { eq: $path }) {
       cover {
         image {
           childImageSharp {
@@ -61,12 +61,13 @@ export const pageQuery = graphql`
         }
       }
       name
-      comments {
-        content
-        }
       childMarkdownRemark {
         html
       }
     }
   }
-`
+`;
+// below name in pageQuery
+// comments {
+//   content
+//   }
